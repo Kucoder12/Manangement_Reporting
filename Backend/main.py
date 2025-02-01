85262811004ad14153a02245476e1292f95c62a4
israel23j <israel23j@gmail.com>
2025-02-01 13:34:50 +0100

    update

--- Backend/main.py
+++ Backend/main.py
@@ -4,8 +4,11 @@ from database.base_de_datos import Connection_Database
 
 
 myapp = FastAPI()
+myapp.title = "Good Service API"
 
-myapp.post('/users')
-async def new_user(name:str, last_name:str, email:str, phone:int, role:str, database=Depends(get_db)):
+myapp.get('/users', tags=['Users'])
+
+myapp.post('/users', Form)
+async def new_user(name:str, last_name:str, email:str, phone:int, role:str, database=Depends(get_db), tags=['Users']):
     database.add_employe(name,last_name,email, phone,role)
     return {"message":"Se ha insertado el empleado correctamente"}
